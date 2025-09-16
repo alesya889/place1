3264a1cb2bbec0eb921e1173e0870b09359cc1be
alesya <a.davydova5@g.nsu.ru>
2025-09-16 10:15:11 +0700

    Add files via upload

--- Alesya/1pract.py
+++ Alesya/1pract.py
@@ -1,3 +1,1 @@
-a=(' Ace of Base - All That She Wants' ,'No Doubt - Dont Speak ','Bad Boys Blue - Youre A Woman ',' E-Type - Angels Crying',' Haddaway - What Is Love ')
-for i in a[::-1]:
-    print(i)
+print('23424')
