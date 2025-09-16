4d54b2b96566779f2f8a8bd66fc2685461607539
alesya889 <a.davydova5@g.nsu.ru>
2025-09-16 09:58:20 +0700

    1
    dfba

---
+++ Alesya/1pract.py
@@ -0,0 +1,3 @@
+a=(' Ace of Base - All That She Wants' ,'No Doubt - Dont Speak ','Bad Boys Blue - Youre A Woman ',' E-Type - Angels Crying',' Haddaway - What Is Love ')
+for i in a[::-1]:
+    print(i)
